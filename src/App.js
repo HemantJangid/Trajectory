e8ce478d7ccf63474e109b320a374bd22651ec48
HemantJangid <jhemant539@gmail.com>
2021-03-06 19:24:16 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -35,102 +35,109 @@ function App() {
   return (
     <div className="container">
       <div className="left">
-        <div className="controls">
-          <h1 style={{ marginBottom: "50px" }}>Visualizer</h1>
-          <p>River's Velocity(m/s)</p>
-          <div className="control">
-            <button
-              onClick={() =>
-                setValues({
-                  ...values,
-                  vr: values.vr === 1 ? values.vr : values.vr - 1,
-                })
-              }
-            >
-              -
-            </button>{" "}
-            <h4>{values.vr}</h4>{" "}
-            <button onClick={() => setValues({ ...values, vr: values.vr + 1 })}>
-              +
-            </button>
+        <h1 style={{ marginBottom: "50px" }}>Visualizer</h1>
+        <div className="controls-container">
+          <div className="controls">
+            <p>River's Velocity(m/s)</p>
+            <div className="control">
+              <button
+                onClick={() =>
+                  setValues({
+                    ...values,
+                    vr: values.vr === 1 ? values.vr : values.vr - 1,
+                  })
+                }
+              >
+                -
+              </button>{" "}
+              <h4>{values.vr}</h4>{" "}
+              <button
+                onClick={() => setValues({ ...values, vr: values.vr + 1 })}
+              >
+                +
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="controls">
-          <p>Man's Velocity(m/s)</p>
-          <div className="control">
-            <button
-              onClick={() =>
-                setValues({
-                  ...values,
-                  vm: values.vm === 1 ? values.vm : values.vm - 1,
-                })
-              }
-            >
-              -
-            </button>{" "}
-            <h4>{values.vm}</h4>{" "}
-            <button onClick={() => setValues({ ...values, vm: values.vm + 1 })}>
-              +
-            </button>
+          <div className="controls">
+            <p>Man's Velocity(m/s)</p>
+            <div className="control">
+              <button
+                onClick={() =>
+                  setValues({
+                    ...values,
+                    vm: values.vm === 1 ? values.vm : values.vm - 1,
+                  })
+                }
+              >
+                -
+              </button>{" "}
+              <h4>{values.vm}</h4>{" "}
+              <button
+                onClick={() => setValues({ ...values, vm: values.vm + 1 })}
+              >
+                +
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="controls">
-          <p>Jump Angle(degrees)</p>
-          <div className="control">
-            <button
-              onClick={() =>
-                setValues({
-                  ...values,
-                  angle: values.angle === 1 ? values.angle : values.angle - 1,
-                })
-              }
-            >
-              -
-            </button>{" "}
-            <h4>{values.angle}</h4>{" "}
-            <button
-              onClick={() =>
-                setValues({
-                  ...values,
-                  angle: values.angle === 90 ? values.angle : values.angle + 1,
-                })
-              }
-            >
-              +
-            </button>
+          <div className="controls">
+            <p>Jump Angle(degrees)</p>
+            <div className="control">
+              <button
+                onClick={() =>
+                  setValues({
+                    ...values,
+                    angle: values.angle === 1 ? values.angle : values.angle - 1,
+                  })
+                }
+              >
+                -
+              </button>{" "}
+              <h4>{values.angle}</h4>{" "}
+              <button
+                onClick={() =>
+                  setValues({
+                    ...values,
+                    angle:
+                      values.angle === 90 ? values.angle : values.angle + 1,
+                  })
+                }
+              >
+                +
+              </button>
+            </div>
           </div>
-        </div>
-        <div className="controls">
-          <p>Width of river(m)</p>
-          <div className="control">
-            <button
-              onClick={() =>
-                setValues({
-                  ...values,
-                  rw: values.rw === 1 ? values.rw : values.rw - 1,
-                })
-              }
-            >
-              -
-            </button>{" "}
-            <h4>{values.rw}</h4>{" "}
-            <button
-              onClick={() =>
-                setValues({
-                  ...values,
-                  rw: values.rw >= 11 ? values.rw : values.rw + 1,
-                })
-              }
-            >
-              +
-            </button>
+          <div className="controls">
+            <p>Width of river(m)</p>
+            <div className="control">
+              <button
+                onClick={() =>
+                  setValues({
+                    ...values,
+                    rw: values.rw === 1 ? values.rw : values.rw - 1,
+                  })
+                }
+              >
+                -
+              </button>{" "}
+              <h4>{values.rw}</h4>{" "}
+              <button
+                onClick={() =>
+                  setValues({
+                    ...values,
+                    rw: values.rw >= 11 ? values.rw : values.rw + 1,
+                  })
+                }
+              >
+                +
+              </button>
+            </div>
           </div>
-        </div>
 
-        <div className="controls">
-          <p>Total time taken (s)</p>
-          <div className="control">
-            <h1>{totalTime}</h1>
+          <div className="controls">
+            <p>Total time taken (s)</p>
+            <div className="control">
+              <h1>{totalTime}</h1>
+            </div>
           </div>
         </div>
       </div>
